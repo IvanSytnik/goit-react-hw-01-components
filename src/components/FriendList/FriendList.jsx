35c1b6cbd09327aa94beb0e22b3472d46058c689
Ivan Sytnik <ivan.sytnik02@gmail.com>
2023-01-05 17:55:23 +0200

    add FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,30 @@
 import css from './FriendList.module.css'
 import PropTypes from 'prop-types';
 
+const FriendListItem = ({avatar, name, isOnline, key}) => {
+  return(
+    <li className={css.friends__item} key={key}>
+        <span className={`${css.friends__status} ${css[isOnline]}`}>{isOnline}</span>
+        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.friends__name}>{name}</p>
+      </li>
+  )
+}
+
 const FriendList = ({friends}) => {
   console.log(friends)
     return(
       <div className={css.friends}>
         <ul className={css.friends__list}>
-         {friends.map(friend => (
-        <li className={css.friends__item} key={friend.id}>
-        <span className={`${css.friends__status} ${css[friend.isOnline]}`}>{friend.isOnline}</span>
-        <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
-        <p className={css.friends__name}>{friend.name}</p>
-      </li>
-    ))}
+        {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    
         </ul>
         </div>
     )
